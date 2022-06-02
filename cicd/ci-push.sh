dc3b4d60f49c4b10ece15c5ab49f1f42c4e7f7bd
Mustafa Güler <mustafa.guler@ciceksepeti.com>
2022-06-02 18:38:52 +0300

    echo for errors

--- cicd/ci-push.sh
+++ cicd/ci-push.sh
@@ -13,28 +13,27 @@ BUILDNUMBER=$BUILD_NUMBER
 TAG=$BRANCHNAME-$BUILDNUMBER
 
 #AWS image push
+echo "ecr login"
 eval $(aws ecr get-login --region eu-central-1)
-
+echo "docker tag for ecr image"
 docker tag $NAME $REMOTE/$NAME:$TAG
-
+echo "docker push to ecr"
 docker push $REMOTE/$NAME:$TAG
-
+echo "ecr logout"
 docker logout https://$REMOTE
 
 
 #GCP image push
+echo "gcr login"
 echo $GCLOUD_SERVICE_KEY | base64 --decode > /tmp/keyfile.json
 
 gcloud auth activate-service-account --key-file=/tmp/keyfile.json
 
 gcloud auth configure-docker
+echo "docker tag for gcr image"
+docker tag $NAME $GCR_REGISTRY/$NAME:$TAG
 
-if [[ $(docker --version | awk '{print $3}')  == 17* ]]; then
-	docker tag $NAME $GCR_REGISTRY/$NAME:$TAG
-else
-	docker tag $NAME $GCR_REGISTRY/$NAME:$TAG
-fi
-
+echo "docker push to gcr"
 docker push $GCR_REGISTRY/$NAME:$TAG
-
+echo "gcr logout"
 docker logout https://$GCR_REGISTRY
