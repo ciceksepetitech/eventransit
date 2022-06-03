e735ff461771a574290e0278b1a9083fe5451a55
Mustafa Güler <mustafa.guler@ciceksepeti.com>
2022-06-03 13:54:16 +0300

    ecr added

--- scripts/ci-push.sh
+++ scripts/ci-push.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 set -e
 
+
+AWS_REGISTRY=$ECR_REGISTRY_ID
 GCR_REGISTRY=$GCR_REGISTRY_ID
 NAME=ciceksepeti/eventransit
 BRANCHNAME=$(git branch | grep \* | cut -d ' ' -f2 | tr '/' '-' | tr '[:upper:]' '[:lower:]' )
 BRANCH=${GITHUB_REF##*/}
 BUILDNUMBER=$BUILD_NUMBER
-
-
 TAG=$BRANCHNAME-$BUILDNUMBER
+
+
+
+#AWS image push
+echo "ecr login"
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $AWS_REGISTRY
+echo "docker tag for ecr image"
+docker tag $NAME $AWS_REGISTRY/$NAME:$TAG
+echo "docker push to ecr"
+docker push $AWS_REGISTRY/$NAME:$TAG
+echo "ecr logout"
+docker logout https://$REMOTE
+
+
 
 #GCP image push
 echo "gcr login"
-
 echo $GCLOUD_SERVICE_KEY | base64 -di > /tmp/keyfile.json
-
 gcloud auth activate-service-account --key-file=/tmp/keyfile.json
-
 gcloud auth configure-docker
 echo "docker tag for gcr image"
 docker tag $NAME $GCR_REGISTRY/$NAME:$TAG
-
 echo "docker push to gcr"
 docker push $GCR_REGISTRY/$NAME:$TAG
 echo "gcr logout"
