248895a5314b2f6b1d34658b787d5ee2615185df
Mennan Kose <mennan.kose@ciceksepeti.com>
2022-06-17 14:48:38 +0300

    Removed unnecessary lines

--- scripts/ci-docker-entrypoint.sh
+++ scripts/ci-docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-# if  [ -n "$ASPNETCORE_ENVIRONMENT" ] && [ "$ASPNETCORE_ENVIRONMENT" != "Development" ]; then envtpl -in ./appsettings.json.tmpl > ./appsettings.$ASPNETCORE_ENVIRONMENT.json; fi
-
 # Overwrite shell process with the dotnet process using exec system call.
 # exec call replaces shell process with the dotnet process so that it runs as PID 1.
 # It is due to the fact that container engine sends the signals (SIGTERM) to PID 1.
