1eb32fb44abec1094a085a48e7aeec0b176fc6fd
kaan.celebi <kaan.celebi@ciceksepeti.com>
2022-10-19 00:13:48 +0300

    bson guid representation added

--- src/EvenTransit.Data.MongoDb/Repositories/BaseMongoRepository.cs
+++ src/EvenTransit.Data.MongoDb/Repositories/BaseMongoRepository.cs
@@ -1,5 +1,6 @@
-using EvenTransit.Data.MongoDb.Settings;
+﻿using EvenTransit.Data.MongoDb.Settings;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Conventions;
 using MongoDB.Driver;
 
@@ -14,11 +15,11 @@ public class BaseMongoRepository<T>
     {
         var connectionString = mongoDbConnectionStringBuilder.ConnectionString;
 
-        var pack = new ConventionPack {new IgnoreExtraElementsConvention(true)};
+        var pack = new ConventionPack { new IgnoreExtraElementsConvention(true) };
         ConventionRegistry.Register("EvenTransit Conventions", pack, t => true);
-        
+
         var client = new MongoClient(connectionString);
         var database = client.GetDatabase(mongoDbSettings.Value.Database);
-        Collection = database.GetCollection<T>(typeof(T).Name);
+        Collection = database.GetCollection<T>(typeof(T).Name, new MongoCollectionSettings { GuidRepresentation = GuidRepresentation.Standard });
     }
 }
