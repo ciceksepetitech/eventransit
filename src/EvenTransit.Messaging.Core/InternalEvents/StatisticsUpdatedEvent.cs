e40cff43daa120b131d4ff331972207b5e4aef13
Mümin Varıcı <mumin.varici@ciceksepeti.com>
2023-01-23 16:34:14 +0300

    Revert "#feat logs-screen internal event publisher"
    This reverts commit 8d91641ba4fa8dbd756f3a1d98502b14faef24df.
    
    # Conflicts:
    #	src/EvenTransit.Service/BackgroundServices/SubscriptionService.cs

--- src/EvenTransit.Messaging.Core/InternalEvents/StatisticsUpdatedEvent.cs
+++
@@ -1,9 +0,0 @@
-using EvenTransit.Domain.Entities;
-using EvenTransit.Messaging.Core.Abstractions;
-
-namespace EvenTransit.Messaging.Core.InternalEvents;
-
-public class StatisticsUpdatedEvent : IInternalAsyncEvent
-{
-    public Logs Details { get; set; }
-}
--- src/EvenTransit.Service/ServiceCollectionExtensions.cs
+++ src/EvenTransit.Service/ServiceCollectionExtensions.cs
@@ -1,15 +1,11 @@
+using System.Reflection;
 using EvenTransit.Messaging.Core.Abstractions;
 using EvenTransit.Messaging.Core.Domain;
-using EvenTransit.Messaging.Core.InternalEvents;
 using EvenTransit.Service.Abstractions;
-using EvenTransit.Service.Abstractions.Events;
-using EvenTransit.Service.BackgroundServices;
-using EvenTransit.Service.InternalEvents;
 using EvenTransit.Service.Locker;
 using EvenTransit.Service.Mappers;
 using EvenTransit.Service.Services;
 using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
 
 namespace EvenTransit.Service;
 
@@ -24,7 +20,7 @@ public static class ServiceCollectionExtensions
             services.AddScoped<IHttpProcessor, HttpProcessor>();
             services.AddScoped<IDistributedLocker, DistributedLocker>();
         }
-
+        
         services.AddSingleton<IRetryQueueHelper, RetryQueueHelper>();
 
         return services;
@@ -33,30 +29,16 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddServices(this IServiceCollection services, bool modeConsumer)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
+        
         if (modeConsumer)
         {
             services.AddScoped<IEventService, EventService>();
             services.AddScoped<ILogService, LogService>();
             services.AddTransient<ICustomObjectMapper, CustomObjectMapper>();
         }
-
+        
         services.AddScoped<IEventPublisherService, EventPublisherService>();
-        services.AddAsyncRunners();
-
+        
         return services;
-    }
-
-    /// <summary>
-    /// Register async runner that runs actions with new lifetimescope.
-    /// </summary>
-    /// <param name="services">IServiceCollection</param>
-    /// <returns>Collection of service descriptors</returns>
-    private static void AddAsyncRunners(this IServiceCollection services)
-    {
-        services.AddScoped<IAsyncRunner, ScopeSafeAsyncRunner>();
-        services.AddScoped<IInternalEventPublisher, InternalEventPublisher>();
-        services.AddScoped<ISubscriptionService, SubscriptionService>();
-        services.AddScoped<IAsyncConsumer<StatisticsUpdatedEvent>, StatisticsUpdatedEventConsumer>();
     }
 }
