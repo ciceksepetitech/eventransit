6b94e28712df0de0d20a4128c07f69ab495761b4
kaan-celebi <111117055+kaan-celebi@users.noreply.github.com>
2023-10-05 11:27:35 +0300

    Update ci-build.sh

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -7,6 +7,7 @@ echo "Build starting"
 
 
 docker build \
+    --build-arg NEW_RELIC_LICENSE_KEY="$NEW_RELIC_LICENSE_KEY" \
     --rm=false --file "$DOCKERFILE" -t ciceksepeti/eventransit .
 
 echo "Build finished"
