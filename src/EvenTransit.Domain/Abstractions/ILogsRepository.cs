92c4f08cfc160fb13637489b89fc59b4d061ba53
kaan.celebi <kaan.celebi@ciceksepeti.com>
2025-05-29 11:09:31 +0300

    date removed from count query

--- src/EvenTransit.Domain/Abstractions/ILogsRepository.cs
+++ src/EvenTransit.Domain/Abstractions/ILogsRepository.cs
@@ -8,5 +8,5 @@ public interface ILogsRepository
     Task InsertLogAsync(Logs model);
     Task<LogFilter> GetLogsAsync(Expression<Func<Logs, bool>> predicate, string requestBodyRegex, int page);
     Task<Logs> GetByIdAsync(Guid id);
-    (long, long) GetLogsCountByEvent(string eventName, string serviceName, DateTime startDate);
+    (long, long) GetLogsCountByEvent(string eventName, string serviceName);
 }
